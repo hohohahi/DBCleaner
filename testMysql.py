bc1728cef59e3cb457b921c406964ad11df3470f
hohohahi <hohohahi@qq.com>
2016-02-02 15:35:23 +0800

    1. initial submit.

---
+++ testMysql.py
@@ -0,0 +1,10 @@
+import MySQLdb
+
+try:
+    conn=MySQLdb.connect(host='109.205.92.50',user='om',passwd='bestbrainer',db='oddsmatrixdb',port=3306)
+    cur=conn.cursor()
+    cur.execute('select count(*) from Location_history')
+    cur.close()
+    conn.close()
+except MySQLdb.Error,e:
+     print "Mysql Error %d: %s" % (e.args[0], e.args[1])
